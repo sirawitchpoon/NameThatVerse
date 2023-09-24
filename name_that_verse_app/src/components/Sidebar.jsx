327a3f120d45bc976399793103c73bf01da7d454
Sirawitch <sirawitchbu@kkumail.com>
2023-09-25 06:10:41 +0700

    Music Streaming 101

---
+++ name_that_verse_app/src/components/Sidebar.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import styled from "styled-components";
+import { MdHomeFilled, MdSearch } from "react-icons/md";
+import { IoLibrary } from "react-icons/io5";
+import Playlists from "./Playlists";
+export default function Sidebar() {
+  return (
+    <Container>
+      <div className="top__links">
+        <div className="logo">
+          <img
+            src="https://www.coolgenerator.com/Data/Textdesign/202309/6fa9ce1780f20c606413b25460ab8578.png"
+            alt="spotify"
+          />
+        </div>
+        
+      </div>
+      <Playlists />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  background-color: black;
+  color: #b3b3b3;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+  .top__links {
+    display: flex;
+    flex-direction: column;
+    .logo {
+      text-align: center;
+      margin: 1rem 0;
+      img {
+        max-inline-size: 80%;
+        block-size: auto;
+      }
+    }
+    ul {
+      list-style-type: none;
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+      padding: 1rem;
+      li {
+        display: flex;
+        gap: 1rem;
+        cursor: pointer;
+        transition: 0.3s ease-in-out;
+        &:hover {
+          color: white;
+        }
+      }
+    }
+  }
+`;
